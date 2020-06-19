2882faca6c1e3575defa9e247e084ad38e96ff78
GuRuGu <peter.k.nets@gmail.com>
2020-06-19 18:37:21 +0300

    Write nicely formatted report

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -8,4 +8,10 @@ axios.get(url).then(response => {
   const ID = todo.ID;
   const title = todo.Title;
   const finished = todo.finished;
+
+  console.log(`
+    The Todo with ID: ${ID}
+    Has a title of: ${title}
+    Is it finished? ${finished}
+  `);
 });
