0d280f63746538f9e0e77d70b44c24a052168684
GuRuGu <peter.k.nets@gmail.com>
2020-07-08 20:02:02 +0300

    Add type guard for array

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -7,11 +7,13 @@ class Sorter {
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
         // If collection is an array of numbers
-        if (this.collection[j] > this.collection[j + 1]) {
-          [this.collection[j], this.collection[j + 1]] = [
-            this.collection[j + 1],
-            this.collection[j],
-          ];
+        if (this.collection instanceof Array) {
+          if (this.collection[j] > this.collection[j + 1]) {
+            [this.collection[j], this.collection[j + 1]] = [
+              this.collection[j + 1],
+              this.collection[j],
+            ];
+          }
         }
 
         // If collection is a string, do this logic instead:
