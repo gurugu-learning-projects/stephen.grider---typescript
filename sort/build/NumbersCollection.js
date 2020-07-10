1f87dbadf056a8f36bf77b0e979527e60ac8fb1d
GuRuGu <peter.k.nets@gmail.com>
2020-07-10 21:05:15 +0300

    Implement compare method inside NumbersCollection

--- sort/build/NumbersCollection.js
+++ sort/build/NumbersCollection.js
@@ -5,6 +5,9 @@ var NumbersCollection = /** @class */ (function () {
     function NumbersCollection(data) {
         this.data = data;
     }
+    NumbersCollection.prototype.compare = function (leftIndex, rightIndex) {
+        return this.data[leftIndex] > this.data[rightIndex];
+    };
     return NumbersCollection;
 }());
 exports.NumbersCollection = NumbersCollection;
