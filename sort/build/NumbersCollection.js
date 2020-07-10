50dfd65b91315ac6fdf9864bc80b00a73d9d9605
GuRuGu <peter.k.nets@gmail.com>
2020-07-10 21:03:04 +0300

    Create NumbersCollection class with a simple constructor

---
+++ sort/build/NumbersCollection.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NumbersCollection = void 0;
+var NumbersCollection = /** @class */ (function () {
+    function NumbersCollection(data) {
+        this.data = data;
+    }
+    return NumbersCollection;
+}());
+exports.NumbersCollection = NumbersCollection;
