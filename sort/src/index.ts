6b322ec6a2d1252092b7d56984d07b52a508d9f4
GuRuGu <peter.k.nets@gmail.com>
2020-07-15 17:32:54 +0300

    Rewrite collection classes to extend Sorter abstract class

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,24 +1,19 @@
-import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
 
-// const numbers = new NumbersCollection([10, 3, -5, 0, -29]);
-// const sorter = new Sorter(numbers);
-// sorter.sort();
-// console.log(numbers.data);
+const numbers = new NumbersCollection([10, 3, -5, 0, -29]);
+numbers.sort();
+console.log(numbers.data);
 
-// const chars = new CharactersCollection("XlZkyjDSFjkljsdfAA");
-// const sorter = new Sorter(chars);
-// sorter.sort();
-// console.log(chars.data);
+const chars = new CharactersCollection("XlZkyjDSFjkljsdfAA");
+chars.sort();
+console.log(chars.data);
 
 const linkedList = new LinkedList();
 linkedList.add(500);
 linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(4);
-
-const sorter = new Sorter(linkedList);
-sorter.sort();
+linkedList.sort();
 linkedList.print();
