0d6ff8a47721d172d7af3769bc99f4bbb66a1029
GuRuGu <peter.k.nets@gmail.com>
2020-07-15 16:14:13 +0300

    Try out sorting a linked list

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,13 +1,24 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 // const numbers = new NumbersCollection([10, 3, -5, 0, -29]);
 // const sorter = new Sorter(numbers);
 // sorter.sort();
 // console.log(numbers.data);
 
-const chars = new CharactersCollection("XlZkyjDSFjkljsdfAA");
-const sorter = new Sorter(chars);
+// const chars = new CharactersCollection("XlZkyjDSFjkljsdfAA");
+// const sorter = new Sorter(chars);
+// sorter.sort();
+// console.log(chars.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(chars.data);
+linkedList.print();
