fd6349c4a27cde50ef81ce3bd7ddac742e3f31e0
GuRuGu <peter.k.nets@gmail.com>
2020-07-20 17:54:28 +0300

    Use dateStringToDate inside CsvFileReader

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,5 +1,5 @@
 import fs from "fs";
-
+import { dateStringToDate } from "./utils";
 export class CsvFileReader {
   data: string[][] = [];
 
@@ -13,6 +13,9 @@ export class CsvFileReader {
       .split("\n")
       .map((row: string): string[] => {
         return row.split(",");
+      })
+      .map((row: string[]): any => {
+        return [dateStringToDate(row[0])];
       });
   }
 }
