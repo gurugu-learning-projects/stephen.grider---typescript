a313950fac803101d373c342ca41de0b3c7c2db4
GuRuGu <peter.k.nets@gmail.com>
2020-07-20 18:05:52 +0300

    Use MatchData tuple to define data

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -5,7 +5,7 @@ import { MatchResult } from "./MatchResult";
 type MatchData = [Date, string, string, number, number, MatchResult, string];
 
 export class CsvFileReader {
-  data: string[][] = [];
+  data: MatchData[] = [];
 
   constructor(public filename: string) {}
 
