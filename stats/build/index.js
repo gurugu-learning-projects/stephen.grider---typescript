dc691640a21ee2a803766a2abe02baaab5440a19
GuRuGu <peter.k.nets@gmail.com>
2020-07-27 16:31:16 +0300

    Implement WinsAnalysis inner logic

--- stats/build/index.js
+++ stats/build/index.js
@@ -2,21 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var MatchReader_1 = require("./MatchReader");
 var CsvFileReader_1 = require("./CsvFileReader");
-var MatchResult_1 = require("./MatchResult");
 var csvFileReader = new CsvFileReader_1.CsvFileReader("football.csv");
 var matchReader = new MatchReader_1.MatchReader(csvFileReader);
 matchReader.load();
-var manUnitedWins = 0;
-var teamWinsHome = function (match, teamName) {
-    return match[1] === teamName && match[5] === MatchResult_1.MatchResult.HomeWin;
-};
-var teamWinsAway = function (match, teamName) {
-    return match[2] === teamName && match[5] === MatchResult_1.MatchResult.AwayWin;
-};
-for (var _i = 0, _a = matchReader.matches; _i < _a.length; _i++) {
-    var match = _a[_i];
-    if (teamWinsHome(match, "Man United") || teamWinsAway(match, "Man United")) {
-        manUnitedWins++;
-    }
-}
 console.log("Man United won " + manUnitedWins + " games");
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -8,20 +8,4 @@ const csvFileReader = new CsvFileReader("football.csv");
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-let manUnitedWins = 0;
-
-const teamWinsHome = (match: MatchData, teamName: string): boolean => {
-  return match[1] === teamName && match[5] === MatchResult.HomeWin;
-};
-
-const teamWinsAway = (match: MatchData, teamName: string): boolean => {
-  return match[2] === teamName && match[5] === MatchResult.AwayWin;
-};
-
-for (let match of matchReader.matches) {
-  if (teamWinsHome(match, "Man United") || teamWinsAway(match, "Man United")) {
-    manUnitedWins++;
-  }
-}
-
 console.log(`Man United won ${manUnitedWins} games`);
