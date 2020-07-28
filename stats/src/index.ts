efd60ae28de012392895d36e41294cd8f229d2c2
GuRuGu <peter.k.nets@gmail.com>
2020-07-28 16:37:51 +0300

    Implement HtmlReport and use it

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,7 +1,15 @@
 import { MatchReader } from "./MatchReader";
 import { CsvFileReader } from "./CsvFileReader";
+import { ConsoleReport } from "./reportTargets/ConsoleReport";
+import { HtmlReport } from "./reportTargets/HtmlReport";
+import { WinsAnalysis } from "./analyzers/WinsAnalysis";
+import { Summary } from "./Summary";
 
 const csvFileReader = new CsvFileReader("football.csv");
 
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
+
+const summary = new Summary(new WinsAnalysis("Man United"), new HtmlReport());
+
+summary.buildAndPrintReport(matchReader.matches);
