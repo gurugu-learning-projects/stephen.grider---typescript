1ada754e77c56a782ccefcca80de5ad8a0f7d16c
GuRuGu <peter.k.nets@gmail.com>
2020-07-30 18:35:14 +0300

    Add a static method to Summary for console report

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -2,7 +2,7 @@ import { MatchReader } from "./MatchReader";
 import { Summary } from "./Summary";
 
 const matchReader = MatchReader.fromCsv("football.csv");
-const summary = Summary.winsAnalysisWithHtmlReport("Man United");
+const summary = Summary.winsAnalysisWithConsoleReport("Man United");
 
 matchReader.load();
 summary.buildAndPrintReport(matchReader.matches);
