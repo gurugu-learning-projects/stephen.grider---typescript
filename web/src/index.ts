7cbd2c48175c95e68a347f79c110100fb2a4e82e
GuRuGu <peter.k.nets@gmail.com>
2020-08-05 19:01:21 +0300

    Implement setter for User class

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,5 +2,7 @@ import { User } from "./models/User";
 
 const user = new User({ name: "Peter", age: 40 });
 
+user.set({ name: "Bob", age: 22 });
+
 console.log(user.get("name"));
 console.log(user.get("age"));
