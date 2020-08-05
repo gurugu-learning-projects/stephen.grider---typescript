791932d27ed3ca954d1063a1dce17661a9d4f468
GuRuGu <peter.k.nets@gmail.com>
2020-08-05 19:08:10 +0300

    Initialize and set zero or single user properties

--- web/src/index.ts
+++ web/src/index.ts
@@ -3,6 +3,7 @@ import { User } from "./models/User";
 const user = new User({ name: "Peter", age: 40 });
 
 user.set({ name: "Bob", age: 22 });
+user.set({ age: 333 });
 
 console.log(user.get("name"));
 console.log(user.get("age"));
