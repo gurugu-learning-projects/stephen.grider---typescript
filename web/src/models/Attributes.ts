1d3a6f6afab032d624ee3eb56254663bb2881d36
GuRuGu <peter.k.nets@gmail.com>
2020-08-13 20:45:05 +0300

    Update Attributes to use correct types

--- web/src/models/Attributes.ts
+++ web/src/models/Attributes.ts
@@ -1,11 +1,22 @@
+import { UserProps } from "./User";
+
 export class Attributes<T> {
   constructor(private data: T) {}
 
-  get(propName: string): string | number {
-    return this.data[propName];
+  get<K extends keyof T>(key: K): T[K] {
+    return this.data[key];
   }
 
   set(update: T): void {
     Object.assign(this.data, update);
   }
 }
+
+const attrs = new Attributes<UserProps>({
+  id: 5,
+  age: 20,
+  name: "Bob",
+});
+
+const name = attrs.get("name");
+const age = attrs.get("age");
