bf26c291bedce2c33c77376cca430c8d058fd0bf
GuRuGu <peter.k.nets@gmail.com>
2020-08-15 19:56:30 +0300

    Connect Attributes to User class

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,5 +1,6 @@
 import { Eventing } from "./Eventing";
 import { Sync } from "./Sync";
+import { Attributes } from "./Attributes";
 
 const rootUrl = "http://localhost:3000/users";
 
@@ -12,4 +13,9 @@ export interface UserProps {
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  public attributes: Attributes<UserProps>;
+
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
+  }
 }
