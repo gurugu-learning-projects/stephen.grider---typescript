50e3e26c934f6bfc105cecf01500117e0f66e2f5
GuRuGu <peter.k.nets@gmail.com>
2020-08-15 20:24:51 +0300

    Test User methods

--- web/src/index.ts
+++ web/src/index.ts
@@ -4,4 +4,10 @@ import { User } from "./models/User";
 const user = new User({ name: "Pops", age: 0 });
 
 // user.set({ name: "Pops", age: 111 });
-user.save();
+console.log(user.get("name"));
+
+user.on("change", () => {
+  console.log("User was changed");
+});
+
+user.trigger("change");
