cb886c0e671a26795548191fa0688c0f5c0e6e18
GuRuGu <peter.k.nets@gmail.com>
2020-08-22 19:40:08 +0300

    Add static build method to User

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,4 +1,7 @@
 import { Model } from "./Model";
+import { Attributes } from "./Attributes";
+import { ApiSync } from "./ApiSync";
+import { Eventing } from "./Eventing";
 
 const rootUrl = "http://localhost:3000/users";
 
@@ -8,4 +11,12 @@ export interface UserProps {
   age?: number;
 }
 
-export class User extends Model<UserProps> {}
+export class User extends Model<UserProps> {
+  static buildUser(attrs: UserProps): User {
+    return new User(
+      new Attributes<UserProps>(attrs),
+      new Eventing(),
+      new ApiSync<UserProps>(rootUrl)
+    );
+  }
+}
