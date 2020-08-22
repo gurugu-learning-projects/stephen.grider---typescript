6c4de6de15f4f6b57785eb03f3fafda2a20825d7
GuRuGu <peter.k.nets@gmail.com>
2020-08-22 19:49:41 +0300

    Shorten getters inside Model

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,11 +1,11 @@
 import { User } from "./models/User";
 
 // const user = new User({ id: 1 });
-const user = new User({ id: 1, name: "newer name", age: 0 });
+const user = User.buildUser({ id: 1 });
 
-user.on("save", () => {
+user.on("change", () => {
   console.log(user);
 });
 
 // user.set({ name: "Dobbie" });
-user.save();
+user.fetch();
