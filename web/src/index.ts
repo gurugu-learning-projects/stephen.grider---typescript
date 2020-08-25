72e39296d075180eecb9e27d585f2b73cb2c857c
GuRuGu <peter.k.nets@gmail.com>
2020-08-25 20:08:39 +0300

    Test HTML rendering

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,10 +1,4 @@
-import { User, UserProps } from "./models/User";
-import { Collection } from "./models/Collection";
+import { UserForm } from "./views/UserForm";
 
-const collection = User.buildUserCollection();
-
-collection.on("change", () => {
-  console.log(collection);
-});
-
-collection.fetch();
+const userForm = new UserForm(document.getElementById("root"));
+userForm.render();
