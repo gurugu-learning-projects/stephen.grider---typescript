3446f8d86bba61642cc99baede1a19f5a0b564be
GuRuGu <peter.k.nets@gmail.com>
2020-08-26 20:35:32 +0300

    Display user data

--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -1,20 +1,28 @@
+import { User } from "../models/User";
+
 export class UserForm {
-  constructor(public parent: Element) {}
+  constructor(public parent: Element, public model: User) {}
 
   eventsMap(): { [key: string]: () => void } {
     return {
       "click:button": this.onButtonClick,
+      "mouseover:h1": this.onHeaderHover,
     };
   }
 
   onButtonClick(): void {
     console.log("Hi there");
   }
+  onHeaderHover(): void {
+    console.log("H1 was hovered over");
+  }
 
   template(): string {
     return `
       <div>
         <h1>User Form</h1>
+        <div>User name: ${this.model.get("name")}</div>
+        <div>User age: ${this.model.get("age")}</div>
         <input />
         <button>Click me</button>
       </div>
