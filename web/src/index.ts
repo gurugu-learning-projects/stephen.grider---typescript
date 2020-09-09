58f931d764d4972fe01cc680ab9eb3b2c6620374
GuRuGu <peter.k.nets@gmail.com>
2020-09-09 20:37:33 +0300

    Edit and display user data

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,13 +1,18 @@
 import { UserForm } from "./views/UserForm";
+import { UserShow } from "./views/UserShow";
+import { UserEdit } from "./views/UserEdit";
 import { User } from "./models/User";
 
-const user = User.buildUser({ name: "NAME", age: 20 });
+const user = User.buildUser({ name: "Peter", age: 20 });
 
 const root = document.getElementById("root");
 
 if (root) {
-  const userForm = new UserForm(root, user);
-  userForm.render();
+  // const userForm = new UserForm(root, user);
+  const userEdit = new UserEdit(root, user);
+  // userForm.render();
+  userEdit.render();
+  console.log(userEdit);
 } else {
   throw new Error("Root element not found");
 }
