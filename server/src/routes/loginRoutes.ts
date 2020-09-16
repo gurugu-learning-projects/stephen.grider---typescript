056fdfd4be7f82076a35b8b01dd038d3f11f1603
GuRuGu <peter.k.nets@gmail.com>
2020-09-16 19:44:47 +0300

    Add a true protected route with middleware to check authentication

--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -1,8 +1,17 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response, NextFunction } from "express";
 
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined };
 }
+
+const requireAuth = (req: Request, res: Response, next: NextFunction): void => {
+  if (req.session && req.session.loggedIn) {
+    next();
+    return;
+  }
+
+  res.status(403).send("Not permitted");
+};
 
 const router = Router();
 
@@ -33,7 +42,7 @@ router.post("/login", (req: RequestWithBody, res: Response) => {
   }
 });
 
-router.get('/', (req: Request, res: Response) => {
+router.get("/", (req: Request, res: Response) => {
   if (req.session && req.session.loggedIn) {
     res.send(`
       <div>
@@ -51,9 +60,13 @@ router.get('/', (req: Request, res: Response) => {
   }
 });
 
-router.get('/logout', (req: Request, res: Response) => {
+router.get("/logout", (req: Request, res: Response) => {
   req.session = null;
-  res.redirect('/')
-})
+  res.redirect("/");
+});
+
+router.get("/protected", requireAuth, (req: Request, res: Response) => {
+  res.send("Welcome to protected route");
+});
 
 export { router };
