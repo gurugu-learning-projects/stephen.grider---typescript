17bbfbfd4a3418cb56365668860b2b5933d5e0a9
GuRuGu <peter.k.nets@gmail.com>
2020-09-25 20:26:21 +0300

    Turn Router into singleton

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,16 @@
 import express from "express";
-import { router } from "./routes/loginRoutes";
 import cookieSession from "cookie-session";
-import { router as controllerRouter } from "./controllers/decorators/controller";
 
-import "./controllers/loginController";
+import { router } from "./routes/loginRoutes";
+import { AppRouter } from "./AppRouter";
+import "./controllers/LoginController";
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ["fjlkldssd"] }));
 app.use(router);
-app.use(controllerRouter);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
