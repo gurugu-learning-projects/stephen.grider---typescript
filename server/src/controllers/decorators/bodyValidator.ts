53f6f00e66ce153fa9ec11bda33cb4ff5945bf99
GuRuGu <peter.k.nets@gmail.com>
2020-09-29 20:13:31 +0300

    Test body validator decorator-middleware

--- server/src/controllers/decorators/bodyValidator.ts
+++ server/src/controllers/decorators/bodyValidator.ts
@@ -1,7 +1,7 @@
 import "reflect-metadata";
 import { MetadataKeys } from "./MetadataKeys";
 
-export function use(...keys: string[]) {
+export function bodyValidator(...keys: string[]) {
   return function (target: any, key: string, desc: PropertyDescriptor) {
     Reflect.defineMetadata(MetadataKeys.validator, keys, target, key);
   };
--- server/src/controllers/loginController.ts
+++ server/src/controllers/loginController.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { get, controller, use } from "./decorators";
+import { get, controller, bodyValidator, post } from "./decorators";
 
 function logger(req: Request, res: Response, next: NextFunction) {
   console.log("Request was made!!!");
@@ -9,7 +9,6 @@ function logger(req: Request, res: Response, next: NextFunction) {
 @controller("/auth")
 class LoginController {
   @get("/login")
-  @use(logger)
   getLogin(req: Request, res: Response): void {
     res.send(`
       <form method="POST">
@@ -24,5 +23,18 @@ class LoginController {
         <button>Submit</button>
       </form>
     `);
+  }
+
+  @post("/login")
+  @bodyValidator("email", "password")
+  postLogin(req: Request, res: Response) {
+    const { email, password } = req.body;
+
+    if (email && password && email === "hi@hi.com" && password === "123456") {
+      req.session = { loggedIn: true };
+      res.redirect("/");
+    } else {
+      res.send("Invalid email or password");
+    }
   }
 }
