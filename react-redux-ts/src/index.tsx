07103867e0decf7f649cc5c86086f4e0b738822f
GuRuGu <peter.k.nets@gmail.com>
2020-10-03 14:23:31 +0300

    Revert back to class-based component

--- react-redux-ts/src/index.tsx
+++ react-redux-ts/src/index.tsx
@@ -5,31 +5,28 @@ interface AppProps {
   color?: string;
 }
 
-const App = (props: AppProps): JSX.Element => {
-  return <div>{props.color}</div>;
-};
-// class App extends React.Component<AppProps> {
-//   state = {
-//     counter: 0,
-//   };
+class App extends React.Component<AppProps> {
+  state = {
+    counter: 0,
+  };
 
-//   onIncrement = (): void => {
-//     this.setState({ counter: this.state.counter + 1 });
-//   };
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
 
-//   onDecrement = (): void => {
-//     this.setState({ counter: this.state.counter - 1 });
-//   };
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
 
-//   render() {
-//     return (
-//       <>
-//         <button onClick={this.onIncrement}>Increment</button>
-//         <button onClick={this.onDecrement}>Decrement</button>
-//         <div>{this.state.counter}</div>
-//       </>
-//     );
-//   }
-// }
+  render() {
+    return (
+      <>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        <div>{this.state.counter}</div>
+      </>
+    );
+  }
+}
 
 ReactDOM.render(<App color="red" />, document.getElementById("root"));
